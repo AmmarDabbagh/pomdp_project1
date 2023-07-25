bd6a12eca6363b10c7dcd58321b1cfac58813e37
AmmarDabbagh <ammar1452002@gmail.com>
2023-07-25 22:26:43 +0300

    sec_change

--- firstFormula.py
+++ firstFormula.py
@@ -3,18 +3,18 @@ import numpy as np
 j=[0,1]
 theta=[0,1]
 
-
+#Transition Probabilities
 p0=np.eye(2)
 p1=np.full((2,2), 0.5)
 p2=np.full((2,2), 0.5)
 
-
+#Observation Probabilities
 r0=np.array([[0.85, 0.15], [0.15, 0.85]])
 r1=np.full((2,2), 0.5)
 r2=np.full((2,2), 0.5)
 
 
-
+#Immediate Rewards
 w0=[-1,-1]
 w1=[-100,10]
 w2=[10,-100]
@@ -38,5 +38,4 @@ def getReward(i,a):
     
 print(getReward(1,2))
 
-print("hi")
 

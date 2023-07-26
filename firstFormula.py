936446e92c3702add19c01093f9bacce1d1fe920
AmmarDabbagh <ammar1452002@gmail.com>
2023-07-26 05:49:17 +0300

    First Formula

--- firstFormula.py
+++ firstFormula.py
@@ -8,6 +8,7 @@ p0=np.eye(2)
 p1=np.full((2,2), 0.5)
 p2=np.full((2,2), 0.5)
 
+
 #Observation Probabilities
 r0=np.array([[0.85, 0.15], [0.15, 0.85]])
 r1=np.full((2,2), 0.5)
@@ -22,7 +23,8 @@ w2=[10,-100]
 
 
 def getReward(i,a):
-    resulst=0
+    result=0
+    
     if(a==0):
         p=p0;r=r0;w=w0
     if(a==1):
@@ -30,12 +32,14 @@ def getReward(i,a):
     if(a==2):
         p=p2;r=r2;w=w2
         
+#double summation        
     for ele in j:
         for ele2 in theta:
-            resulst+=(p[i,ele])*(r[ele,ele2])*(w[i])
+            result+=(p[i,ele])*(r[ele,ele2])*(w[i])
         
-    return resulst    
-    
+    return result    
+
+
+
+#to test the function    
 print(getReward(1,2))
-
-
